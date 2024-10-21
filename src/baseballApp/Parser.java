628bb384316d78b7b4f5fbdcf4523e4aad737f6c
ryu <fbqudrlf09@naver.com>
2024-10-21 16:14:01 +0900

    패키지 구성 및 클래스 구분

---
+++ src/baseballApp/Parser.java
@@ -0,0 +1,7 @@
+package baseballApp;
+
+public class Parser {
+
+    // 입력된 값을 검증하는 클래스
+
+}
---
+++ src/baseballApp/Sequnce.java
@@ -0,0 +1,45 @@
+package baseballApp;
+
+import java.util.Scanner;
+
+public class Sequnce {
+
+    // 실제 동작하는 클래스
+    public void run(){
+
+        boolean flag = true;
+
+        while(flag){
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("환영합니다! 원하시는 번호를 입력해주세요");
+            System.out.println("0. 자리수 설정 1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
+            String command = sc.nextLine();
+
+            switch(command){
+                case "0": {
+                    // 자릿수 설정 구현
+
+                    break;
+                }
+                case "1":{
+                    // 게임 시작하기 구현
+
+                    break;
+                }
+                case "2": {
+                    //게임 기록 보기 구현
+
+                    break;
+                }
+                case "3": {
+                    flag = false;
+                    break;
+                }
+                default: {
+
+                }
+            }
+        }
+    }
+}
